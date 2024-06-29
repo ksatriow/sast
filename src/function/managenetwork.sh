e92bbec924418615d63bc4cc99395619ef2df080
wibowo <kukuhsatriowibowo@gmail.com>
2024-06-29 22:11:17 +0700

    function manage network

---
+++ src/function/managenetwork.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+get_network() {
+    echo "List of network interfaces:"
+    ip addr
+}
+
+create_network() {
+    echo "Creating a new network interface..."
+}
+
+update_network() {
+    echo "Updating a network interface..."
+}
+
+delete_network() {
+    echo "Deleting a network interface..."
+}
